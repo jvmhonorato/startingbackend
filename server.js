391b8d9a089b6e74a2ac9979f3a5c07847d598fa
Victor Honorato <101150943+vituhonorato@users.noreply.github.com>
2022-08-19 20:44:14 -0300

    create a basic HTTP server

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+
+//create server on http mode using res and res parameters 
+const server = http.createServer((req,res)=>{
+    res.end('Fullstack Master')
+})
+
+
+//liste the door number 3000
+server.listen(3000)
